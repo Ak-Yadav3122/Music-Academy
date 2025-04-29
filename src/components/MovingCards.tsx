ceb13f43edd4df1d7eff3df379043e2b8480b8c0
Ak-Yadav3122 <aichchhik12@gmail.com>
2025-04-29 22:46:59 +0530

    update the code

--- src/components/MovingCards.tsx
+++ src/components/MovingCards.tsx
@@ -39,7 +39,7 @@ const MovingCards = () => {
   return (
     
     <div className="h-[50rem] w-full dark:bg-black bg-white  dark:bg-grid-small-white/[0.2] bg-grid-small-black/[0.2] relative flex flex-col items-center justify-center overflow-hidden">
-        <h2 className="text-3xl font-bold text-center mb-8 z-10 text-white">Hear our Harmony: Voices of success</h2>
+        <h2 className="text-3xl font-bold text-center mb-8 z-10 text-white">Hear our Harmony: Voices of Success</h2>
         <div className="flex justify-center w-full overflow-hidden px-4 sm:px-6 lg:px-8">
             <div className="w-full max-w-6xl">
             <InfiniteMovingCards
